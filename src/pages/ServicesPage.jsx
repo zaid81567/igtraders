87d8dc40bcb2953a70d8e4a66ffb4dea9f09192b
Md Zaid <zaid81567@gmail.com>
2025-09-24 14:26:55 +0530

    Update ServicesPage.jsx

--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -16,18 +16,19 @@ function ServicesPage() {
     />
 
     <div className="service-page-container">
+      
       <div className="heading">
         <h1>Services we Offer</h1>
       </div>
+      
     </div>
 
-      <div className="all-service-list-container">
-          <div className="service-box">
-              <div className="service-img"> <img src={eWasteRecyclingImg} /> </div>
-              <div className="service-name">E Waste Recycling</div>
-          </div>          
-      </div>
-    
+    <div className="all-service-list-container">
+        <div className="service-box">
+            <div className="service-img"> <img src={eWasteRecyclingImg} /> </div>
+            <div className="service-name">E Waste Recycling</div>
+        </div>          
+     </div>        
     </>
   )
 }
